da3bcd49b399039898831846ef3c207ce4bff6c8
Jake Lundberg <grove.mountain.github@nym.hush.com>
2018-08-13 20:12:03 -0700

    master: updating examples and creation

--- create_tfe_workspace.sh
+++ create_tfe_workspace.sh
@@ -7,16 +7,17 @@
 . ${TFE_ARM_ENV}
 
 # This function will vary by each workspace type.   It's here to allow re-use by environment
+# The function does basic wrapping for setting organization, workspace and category but also accepts any other flags like --sensitive or --hcl 
 create_variables () {
   # AWS specific variables to create
   CATEGORY=terraform
   create_variable name_prefix ${OWNER}-${ENVIRONMENT}
-  create_variable resource_group_location ${REGION}
-  create_variable vn_location ${REGION}
-  create_variable vn_address_space ${VN_ADDRESS_SPACE}
+  create_variable resource_group_location "${REGION}"
+  create_variable vn_location "${REGION}"
+  create_variable vn_address_space ${VN_ADDRESS_SPACE} --hcl
   create_variable sb_address_prefix ${SB_ADDRESS_PREFIX}
   create_variable vm_size ${INSTANCE_TYPE} 
-  create_variable admin_password ${ADMIN_PASSWORD} true
+  create_variable admin_password ${ADMIN_PASSWORD} --sensitive
   create_variable environment_tag ${ENVIRONMENT}
   create_variable owner_tag ${OWNER}
   create_variable ttl_tag ${TTL}
@@ -25,7 +26,7 @@ create_variables () {
   create_variable ARM_SUBSCRIPTION_ID ${ARM_SUBSCRIPTION_ID}
   create_variable ARM_TENANT_ID ${ARM_TENANT_ID}
   create_variable ARM_CLIENT_ID ${ARM_CLIENT_ID}
-  create_variable ARM_CLIENT_SECRET ${ARM_CLIENT_SECRET} true
+  create_variable ARM_CLIENT_SECRET ${ARM_CLIENT_SECRET} --sensitive
   create_variable CONFIRM_DESTROY 1
 }
 
